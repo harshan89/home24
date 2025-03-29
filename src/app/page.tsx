ff9fd320bed14be31b616e59417a29ecbe924677
harshan89 <harshanweb@gmail.com>
2025-03-29 12:55:26 +0530

    Login Component UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import LoginComponent from "@/components/LoginComponent";
 import { fetchProductList } from "@/services/productService";
 import { productListSelector } from "@/state/product/productState";
 import { useEffect } from "react";
@@ -13,21 +14,8 @@ export default function Home() {
   }, []);
 
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <div className="flex flex-col gap-[32px]">
-          {productList.map((product, key) => (
-            <div key={key} className="flex gap-[16px] items-center">
-              <div>
-                <h2 className="text-xl font-bold">{product.name}</h2>
-                <p>{product.description}</p>
-                <p>Price: ${product.price}</p>
-              </div>
-            </div>
-          ))}
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center"></footer>
-    </div>
+    <main>
+      <LoginComponent />
+    </main>
   );
 }
